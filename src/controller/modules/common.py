9fc0b2c07806987c7b49d81b5bfb48658297dfaf
wgdp444 <tatsuyori.shiraga@triple-four.com>
2020-07-09 10:02:52 +0900

    change common.query()
    common.search_query()

--- src/controller/modules/common.py
+++ src/controller/modules/common.py
@@ -1,5 +1,5 @@
 from database import db
-def query(Model, search_condition):
+def search_query(Model, search_condition):
     exprs = [(getattr(Model, name) == value) 
         for name, value in search_condition.items() if value is not None]
     return db.session.query(Model).filter(*exprs).all()
